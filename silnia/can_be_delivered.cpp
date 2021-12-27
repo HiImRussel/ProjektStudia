e710beae573279e9bfa0cf2529e33f5091e32d8e
Russel <russel1234561@gmail.com>
2021-12-27 16:25:27 +0100

    dodano obsulge pracy w weekend

--- silnia/can_be_delivered.cpp
+++ silnia/can_be_delivered.cpp
@@ -10,6 +10,16 @@
 
 using namespace std;
 
+bool checkIfCanBeDelivered(int picked_hour, int picked_minutes, int hours_from, int minutes_from, int hours_to, int minutes_to){
+    if (picked_hour >= hours_from && picked_hour <= hours_to) {
+        if (picked_hour == hours_to && picked_minutes > minutes_to) return false;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 bool canBeDelivered(string delivery_hour) {
     time_t rawtime;
     tm* timeinfo;
@@ -20,23 +30,30 @@ bool canBeDelivered(string delivery_hour) {
 
     if (delivery_hour.length() != 4 && weekday == 7) cout << "nieprawidlowa godzina";
 
-    if (weekday >= 1 && weekday <= 5) {
-        int picked_hour = splitAndConvertStringToNumbers(delivery_hour)[0];
-        int picked_minutes = splitAndConvertStringToNumbers(delivery_hour)[1];
+    //checking if can be delivered in working days
+    int picked_hour = splitAndConvertStringToNumbers(delivery_hour)[0];
+    int picked_minutes = splitAndConvertStringToNumbers(delivery_hour)[1];
 
+    if (weekday >= 1 && weekday <= 5) {
         int working_hour_from = splitAndConvertStringToNumbers(working_days_from)[0];
         int working_minutes_from = splitAndConvertStringToNumbers(working_days_from)[1];
 
         int working_hour_to = splitAndConvertStringToNumbers(working_days_to)[0];
         int working_minutes_to = splitAndConvertStringToNumbers(working_days_to)[1];
 
-        if (picked_hour >= working_hour_from && picked_hour<=working_hour_to) {
-            if (picked_hour == working_hour_to && picked_minutes > working_minutes_to) return false;
-            return true;
-        }
-        else {
-            return false;
-        }
+        checkIfCanBeDelivered(picked_hour, picked_minutes, working_hour_from, working_minutes_from, working_hour_to, working_minutes_to);
     }
-    return false;
+    else if (weekday == 6) {
+        //check if can be delivered in weekend working hours
+        int working_hour_from = splitAndConvertStringToNumbers(weekend_days_from)[0];
+        int working_minutes_from = splitAndConvertStringToNumbers(weekend_days_from)[1];
+
+        int working_hour_to = splitAndConvertStringToNumbers(weekend_days_to)[0];
+        int working_minutes_to = splitAndConvertStringToNumbers(weekend_days_to)[1];
+        
+        checkIfCanBeDelivered(picked_hour, picked_minutes, working_hour_from, working_minutes_from, working_hour_to, working_minutes_to);
+    }
+    else {
+        return false;
+    }
 }
