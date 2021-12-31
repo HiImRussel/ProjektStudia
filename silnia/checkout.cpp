6475e2af6577a8d713b94297836a8bb926c722d7
Samotyja <sekinetsu40k@gmail.com>
2021-12-31 16:26:45 +0100

    Podstawowe wyświetlanie

--- silnia/checkout.cpp
+++ silnia/checkout.cpp
@@ -14,7 +14,6 @@ void checkout() {
 
 	if (!canFoodBeDeliveredAtPickedTime() && delivery_type == 2) {
 		cout << "Nie mozemy zrealizowac z zamowienia w wyznaczonym czasie. Prosze zedytowac swoje zamowienie" << endl;
-		
 		cin.get();
 		cin.get();
 
@@ -23,5 +22,35 @@ void checkout() {
 		exit;
 	}
 
-	cout << "Planowany czas przygotowania dan: " << plannedWaitingTime() << " min";
+	cout << "----------------------------" << endl << "|  Podsumowanie zamowienia  | " << endl << "----------------------------" << endl;
+
+	cout << "Imie: " << user_name << endl;
+	cout << "Nazwisko: " << user_surname << endl << endl;
+
+	switch (delivery_type)
+	{
+	case 1:
+		cout << "Numer stolika: " << table_number << endl << endl;
+	break;
+
+	case 2:
+		cout << "Adres: " << endl;
+		cout << city << endl;
+		cout << street << " " << house_number << endl << endl;
+
+		cout << "Planowane dostarczenie na godzine: " << delivery_hour << endl << endl;
+	break;
+	}
+
+	if (cart_array.size() > 0) {
+		cout << "Zamowione dania: " << endl;
+		for (int i = 0; i < cart_array.size  (); i++) {
+			cout << i + 1 << ". " << "Danie: " << cart_array[i].name << " Ilosc porcji: " << cart_array[i].quantity << " Cena za pojedyncze danie: " << cart_array[i].price << " Cena za wszystkie porcje dania: " << cart_array[i].price * cart_array[i].quantity << endl;
+		}
+	}
+	
+	cout << endl << "Planowany czas przygotowania dan: " << plannedWaitingTime() << " min";
+	
+
+
 }
