886ae3b2d4469adae8ed39a05581d3ae8a908310
Russel <russel1234561@gmail.com>
2022-01-08 23:06:37 +0100

    zmiana nazwy projektu

--- restauracja/global_varaibles.cpp
+++ restauracja/global_varaibles.cpp
@@ -7,6 +7,6 @@ string working_days_from = "08:00";
 string working_days_to = "20:00";
 
 string weekend_days_from = "08:00";
-string weekend_days_to = "18:00";
+string weekend_days_to = "24:00";
 
 int making_time_per_dish = 10;
